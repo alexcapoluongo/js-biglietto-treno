50221e419a2e5ebdb1724aff13b5e078be4f62d5
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-08 10:28:16 +0200

    isNan added

--- js/script.js
+++ js/script.js
@@ -6,29 +6,35 @@ console.log(kmTravel);
 const userAge = parseInt(prompt('inserisci la tua età'));
 console.log(userAge);
 
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-let price = (kmTravel * 0.21);
-console.log(price);
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-let discount;
-
-if (userAge < 18){
-    discount = (price * 0.2);
-    console.log(`your discount is ${discount}`);
-} else if (userAge > 65) {
-    discount = (price * 0.4);
-    console.log(`your discount is ${discount}`);
+if (isNaN(kmTravel) || isNaN(userAge)) {
+    alert('i dati devono essere numeri');
 } else {
-    discount = 0;
-    console.log(`you have to pay a full price`);
+
+    // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    let price = (kmTravel * 0.21);
+    console.log(price);
+    // va applicato uno sconto del 20% per i minorenni
+    // va applicato uno sconto del 40% per gli over 65.
+    let discount;
+
+    if (userAge < 18){
+        discount = (price * 0.2);
+        console.log(`your discount is ${discount}`);
+    } else if (userAge > 65) {
+        discount = (price * 0.4);
+        console.log(`your discount is ${discount}`);
+    } else {
+        discount = 0;
+        console.log(`you have to pay a full price`);
+    }
+
+    // arrotondare a due cifre il prezzo finale
+
+    const finalPrice = (price - discount).toFixed(2);
+    console.log(`your final price is ${finalPrice}`);
+
+    // stampare il risultato
+
+    document.getElementById('result').innerHTML = `the ticket will cost ${finalPrice}`
+
 }
-
-// arrotondare a due cifre il prezzo finale
-
-const finalPrice = (price - discount).toFixed(2);
-console.log(`your final price is ${finalPrice}`);
-
-// stampare il risultato
-
-document.getElementById('result').innerHTML = `the ticket will cost ${finalPrice}`
